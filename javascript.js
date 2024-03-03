d140c042fe14c30f339152ea9f2fb88dee8142cc
Dean Yap <deanyapwj@gmail.com>
2024-03-03 15:17:20 +0800

    Add singleRound() for first round

--- javascript.js
+++ javascript.js
@@ -8,6 +8,14 @@
 // Paper, Scissors, Scissors Wins
 // Scissors, Scissors, Draw
 
+// if (
+//     computerSelection === "rock" ||
+//     computerSelection === "paper" ||
+//     computerSelection === "scissors"
+//   ) {
+//     alert("It's a drawlololol");
+//   }
+
 function getComputerChoice() {
   const game = ["rock", "paper", "scissors"];
   const randomEvent = Math.random();
@@ -15,4 +23,38 @@ function getComputerChoice() {
   return game[choice];
 }
 
-console.log(getComputerChoice());
+const com = getComputerChoice();
+
+// const playerSelection = prompt("rock, paper or scissors?", "");
+
+function singleRound(playerSelection, computerSelection) {
+  computerSelection = com;
+  playerSelection = `${playerSelection.charAt(0).toUpperCase()}${playerSelection
+    .slice(1)
+    .toLowerCase()}`;
+
+  if (playerSelection === "Rock" && computerSelection === "rock") {
+    return `Computer: ${computerSelection.toUpperCase()} It's a draw`;
+  } else if (playerSelection === "Rock" && computerSelection === "paper") {
+    return `Computer: ${computerSelection.toUpperCase()} you lose`;
+  } else if (playerSelection === "Rock" && computerSelection === "scissors") {
+    return `Computer: ${computerSelection.toUpperCase()} you win`;
+  } else if (playerSelection === "Paper" && computerSelection === "paper") {
+    return `Computer: ${computerSelection.toUpperCase()} It"s a draw`;
+  } else if (playerSelection === "Paper" && computerSelection === "rock") {
+    return `Computer: ${computerSelection.toUpperCase()} you win`;
+  } else if (playerSelection === "Paper" && computerSelection === "scissors") {
+    return `Computer: ${computerSelection.toUpperCase()} you lose`;
+  } else if (
+    playerSelection === "Scissors" &&
+    computerSelection === "scissors"
+  ) {
+    return `Computer: ${computerSelection.toUpperCase()} It"s a draw`;
+  } else if (playerSelection === "Scissors" && computerSelection === "paper") {
+    return `Computer: ${computerSelection.toUpperCase()} you win`;
+  } else if (playerSelection === "Scissors" && computerSelection === "rock") {
+    return `Computer: ${computerSelection.toUpperCase()} you lose`;
+  }
+}
+
+console.log(singleRound("scissors"));
